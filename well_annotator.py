76257359911902f576265da46214fafbff9b540d
Harrison Edwards <harrison.edwards@mail.utoronto.ca>
2018-10-03 13:16:54 -0400

    fixed bug and made prettier

--- well_annotator.py
+++ well_annotator.py
@@ -2,14 +2,15 @@ import numpy as np
 import cv2
 import time
 import pickle
+import skimage
+import argparse
 
-img_loc = 'well_image___20_09_2018___12.18.52.033870.tif'
-output_loc = 'out.p'
 
 class WellAnnotator():
 
-    def __init__(self):
-        self.img = cv2.imread(img_loc)
+    def __init__(self, args):
+        args = args
+        self.img = cv2.imread(args.file)
         print(self.img.shape)
         self.grid = False
         self.annotating = False
@@ -17,13 +18,12 @@ class WellAnnotator():
         self.accept_message_displayed = False
         self.y_train = None
         self.x_train = None
-        self.annotation_position_x, self.annotation_position_y = 0,0
+        self.annotation_position_x, self.annotation_position_y = 0, 0
         self.last_x = 0
         self.last_y = 0
         self.zoomed_img = None
 
-
-        self.annotated_img = np.zeros((500, 500,1),dtype = np.uint8)
+        self.annotated_img = np.zeros((500, 500, 1), dtype=np.uint8)
 
         # the values we will use to resize the image at the end to fit the screen
         self.user_view_x = int(1024 * .78)
@@ -31,23 +31,24 @@ class WellAnnotator():
 
         self.y_locs = list(np.arange(0, self.img.shape[0] + 822, 822))
         self.x_locs = list(np.arange(0, self.img.shape[1] + 1024, 1024))
-        self.user_x_index = list(np.arange(len(self.x_locs)-1))
-        self.user_y_index = list(np.arange(len(self.y_locs)-1))
+        self.user_x_index = list(np.arange(len(self.x_locs) - 1))
+        self.user_y_index = list(np.arange(len(self.y_locs) - 1))
 
         self.user_x_locs = list(np.arange(0, self.user_view_x, int(self.user_view_x / len(self.user_x_index))))
         self.user_y_locs = list(np.arange(0, self.user_view_y, int(self.user_view_y / len(self.user_y_index))))
 
         cv2.namedWindow('Annotator')
-        cv2.moveWindow('Annotator',-200,0)
+        cv2.moveWindow('Annotator', -200, 0)
         cv2.setMouseCallback('Annotator', self.mouse_event)
         self.resize_and_show_img(self.img)
         self.start_event_loop()
 
     def write_to_file(self):
-        pickle.dump((self.x_train,self.y_train),open(output_loc,'wb'))
+        fn = args.file.split('.tif')[0] + '_annotated.p'
+        pickle.dump((self.x_train, self.y_train), open(fn, 'wb'))
 
-    def get_indices_from_click(self,x,y):
-        x_index,y_index = 0,0
+    def get_indices_from_click(self, x, y):
+        x_index, y_index = 0, 0
         for loc in self.user_x_locs:
             if x > loc:
                 x_index = loc
@@ -57,18 +58,18 @@ class WellAnnotator():
         x_index = self.user_x_locs.index(x_index)
         y_index = self.user_y_locs.index(y_index)
         # print(x_index,y_index)
-        return x_index,y_index
+        return x_index, y_index
 
-    def get_zoomed_slice(self,x_index,y_index):
+    def get_zoomed_slice(self, x_index, y_index):
         start_y = self.x_locs[x_index]
         start_x = self.y_locs[y_index]
         end_y = self.x_locs[x_index + 1]
         end_x = self.y_locs[y_index + 1]
         self.zoomed_img = np.copy(self.img[start_x:end_x,
-                             start_y:end_y,:])
+                                  start_y:end_y, :])
         return self.zoomed_img
 
-    def resize_mouse_position(self,x,y):
+    def resize_mouse_position(self, x, y):
         annotation_position_x = x / self.user_view_x * self.annotated_img.shape[0]
         annotation_position_y = y / self.user_view_y * self.annotated_img.shape[1]
         return int(annotation_position_x), int(annotation_position_y)
@@ -87,7 +88,8 @@ class WellAnnotator():
         elif event == 0 and flags == 1:
             self.annotated_img[self.annotation_position_y, self.annotation_position_x] = fill_val
             lineThickness = 2
-            cv2.line(self.annotated_img, (self.last_x, self.last_y), (self.annotation_position_x, self.annotation_position_y), fill_val, lineThickness)
+            cv2.line(self.annotated_img, (self.last_x, self.last_y),
+                     (self.annotation_position_x, self.annotation_position_y), fill_val, lineThickness)
             # cv2.line(mask_display, (last_x, last_y), (x, y), (fill_val, fill_val, fill_val), lineThickness)
             self.last_x = self.annotation_position_x
             self.last_y = self.annotation_position_y
@@ -96,19 +98,17 @@ class WellAnnotator():
             self.annotated_img = self.fill_contour(self.annotated_img)
             cv2.imshow('Annotation', self.annotated_img)
 
-
-
     def mouse_event(self, event, x, y, flags, param):
         # we need to find where the user clicked within the grid
         if event == 1 and not self.annotating and not self.zoomed:
             self.zoomed = True
-            x_index, y_index = self.get_indices_from_click(x,y)
-            zoomed_img = self.get_zoomed_slice(x_index,y_index)
+            x_index, y_index = self.get_indices_from_click(x, y)
+            zoomed_img = self.get_zoomed_slice(x_index, y_index)
             self.resize_and_show_img(zoomed_img)
         if self.annotating and self.zoomed:
             self.annotate(event, x, y, flags, param)
 
-    def fill_contour(self,mask):
+    def fill_contour(self, mask):
         fill_val = 255
         im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
@@ -126,6 +126,7 @@ class WellAnnotator():
 
     def end_program(self):
         cv2.destroyAllWindows()
+        self.write_to_file()
         exit()
 
     def toggle_grid(self):
@@ -146,8 +147,8 @@ class WellAnnotator():
         self.resize_and_show_img(img)
 
     def turn_on_annotation(self):
-        cv2.namedWindow(('Annotation'))
-        cv2.moveWindow("Annotation",1000,0)
+        cv2.namedWindow('Annotation')
+        cv2.moveWindow("Annotation", 1000, 0)
         cv2.imshow('Annotation', self.annotated_img)
 
     def toggle_annotation(self):
@@ -160,7 +161,7 @@ class WellAnnotator():
     def display_accept_message(self):
         self.accept_message_displayed = True
         font = cv2.FONT_HERSHEY_SIMPLEX
-        bottomLeftCornerOfText = (10, 500)
+        bottomLeftCornerOfText = (50, 500)
         fontScale = 1
         fontColor = (255, 255, 255)
         lineType = 2
@@ -177,18 +178,17 @@ class WellAnnotator():
         if self.y_train is None:
             self.y_train = self.annotated_img
         else:
-            self.y_train = np.concatenate((self.y_train, self.annotated_img), axis = -1)
-        self.annotated_img = np.zeros((500,500,1), dtype = np.uint8)
+            self.y_train = np.concatenate((self.y_train, self.annotated_img), axis=-1)
+        self.annotated_img = np.zeros((500, 500, 1), dtype=np.uint8)
 
     def add_to_x_train(self):
         img = cv2.cvtColor(self.zoomed_img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img,(500,500))
-        img = np.expand_dims(img,axis = -1)
+        img = skimage.transform.resize(img, (500, 500), anti_aliasing=True)
+        img = np.expand_dims(img, axis=-1)
         if self.x_train is None:
             self.x_train = img
         else:
-            self.x_train = np.concatenate((self.y_train, img), axis=-1)
-
+            self.x_train = np.concatenate((self.x_train, img), axis=-1)
 
     def back_to_finding_mode(self):
         self.annotating = False
@@ -196,12 +196,12 @@ class WellAnnotator():
 
     def reset_annotation(self):
         self.annotating = True
-        self.annotated_img = np.zeros((500,500,1), dtype=np.uint8)
+        self.annotated_img = np.zeros((500, 500, 1), dtype=np.uint8)
         cv2.imshow('Annotation', self.annotated_img)
         self.accept_message_displayed = False
 
     def show_results(self):
-        cv2.imshow('x', self.x_train[:,:,-1])
+        cv2.imshow('x', self.x_train[:, :, -1])
         cv2.imshow('y', self.y_train[:, :, -1])
 
     def accept_annotation(self):
@@ -218,11 +218,11 @@ class WellAnnotator():
 
     def start_event_loop(self):
         event_dict = {
-            -1:self.do_nothing,
-            ord('e'):self.end_program,
-            ord('g'):self.toggle_grid,
-            ord('a'):self.toggle_annotation,
-            ord('y'):self.accept_annotation,
+            -1: self.do_nothing,
+            ord('e'): self.end_program,
+            ord('g'): self.toggle_grid,
+            ord('a'): self.toggle_annotation,
+            ord('y'): self.accept_annotation,
             ord('n'): self.reject_annotation,
             ord('w'): self.write_to_file
         }
@@ -231,19 +231,33 @@ class WellAnnotator():
             if key in event_dict.keys():
                 event_dict[key]()
 
-    def create_grid_img(self,img):
+    def create_grid_img(self, img):
         grid_img = np.copy(img)
         for x in self.x_locs:
-            cv2.line(grid_img, (x, self.y_locs[0]), (x, self.y_locs[-1]) , (255, 255, 255), 25)
+            cv2.line(grid_img, (x, self.y_locs[0]), (x, self.y_locs[-1]), (255, 255, 255), 25)
         for y in self.y_locs:
             cv2.line(grid_img, (self.x_locs[0], y), (self.x_locs[-1], y), (255, 255, 255), 25)
 
         return grid_img
 
 
-
-wa = WellAnnotator()
-
-
-
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''
+    Annotate a well for model training. \n
+    \n
+    Controls:\n
+    g: show grid\n
+    mouse click in grid to view 100x image of that grid location\n
+    a: popup annotation window\n
+    click and drag to circle cells of interest, closed contours will be filled\n
+    a again to bring up accept or deny annotation menu\n
+    y if annotation is good,\n
+    n to reset annotation\n
+    g again to reset view to whole-well\n
+    g again to show grid \n
+    repeat as necessary until all cells have been found and annotated\n
+    w: WRITE BEFORE EXITING!
+    ''')
+    parser.add_argument('-f', '--file', dest='file', help='The well image to annotate.)')
+    args = parser.parse_args()
+    wa = WellAnnotator(args)
